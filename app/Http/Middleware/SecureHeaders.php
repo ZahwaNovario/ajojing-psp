1f1b5abecfae40bb1613895c02ccd50526e17b45
ZahwaNovario <novario120@gmail.com>
2025-07-03 11:51:17 +0700

    Update SecureHeaders.php

--- app/Http/Middleware/SecureHeaders.php
+++ app/Http/Middleware/SecureHeaders.php
@@ -42,8 +42,8 @@ class SecureHeaders
         $viteUrl = "http://{$viteHost}:{$vitePort}";
 
         $policy = "default-src 'self';";
-        $policy .= " script-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net;";
-        $policy .= " style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://fonts.bunny.net https://cdn.jsdelivr.net;";
+        $policy .= " script-src 'self' 'unsafe-inline' $viteUrl https://cdn.jsdelivr.net;";
+        $policy .= " style-src 'self' 'unsafe-inline' $viteUrl https://fonts.googleapis.com https://fonts.bunny.net https://cdn.jsdelivr.net;";
         $policy .= " font-src 'self' https://fonts.gstatic.com https://fonts.bunny.net;";
         $policy .= " img-src 'self' data: https://via.placeholder.com;";
         $policy .= " object-src 'none';";
